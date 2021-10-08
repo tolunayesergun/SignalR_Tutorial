86f6dc1781d21e01851409e8ae92c5e9acc1b1e9
tolunayesergun <tolunay50@gmail.com>
2021-10-09 00:50:48 +0300

    Add project files.

---
+++ WebAPI/Controllers/NotificationController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Hubs;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly IHubContext<Notification> _hubContext;
+
+        public NotificationController(IHubContext<Notification> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendNotification (string message)
+        {
+            //await _hubContext.Clients.All.SendAsync("notify",message);
+            await _hubContext.Clients.Group("5").SendAsync("notify", message);
+            return Ok();
+        }
+    }
+}
---
+++ WebAPI/Hubs/Notification.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Hubs
+{
+    public class Notification : Hub
+    {
+        public async Task AddGroup(string groupId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId,groupId);
+        }
+    }
+}
